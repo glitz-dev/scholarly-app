2940253f702291c00c28a13b486dad75dac15503
glitz-dev <glitzdocuments@gmail.com>
2025-08-12 13:08:17 +0530

    new table added for stroing summerized pdf  info

---
+++ Backend/Scholarly.WebAPI/Model/PDFSummary.cs
@@ -0,0 +1,16 @@
+﻿namespace Scholarly.WebAPI.Model
+{
+    public class PDFSummary
+    {
+        public PDFSummary()
+        {
+
+        }
+        public bool orignial_version { get; set; }
+        public string version_no { get; set; }
+        public string summary { get; set; }
+        public string? pdf_summary_saved_path { get; set; }
+        public bool? is_public { get; set; }
+        
+    }
+}

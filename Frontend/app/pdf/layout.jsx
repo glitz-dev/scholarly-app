3d640e295ddebc1664417778aa39bae6250b4519
glitz-dev <glitzdocuments@gmail.com>
2025-08-28 08:50:40 +0530

    enhanced the user profile ui and mobile view

--- Frontend/app/pdf/layout.jsx
+++ Frontend/app/pdf/layout.jsx
@@ -7,7 +7,7 @@ const Layout = ({ children }) => {
   return (
     <div className="min-h-screen bg-gray-100">
       <Navbar />
-      <main className="min-h-screen px-10 py-5 bg-white dark:bg-gray-800 dark:text-white relative ">
+      <main className="min-h-screen px-2 md:px-10 lg:px-10 py-5 bg-white dark:bg-gray-800 dark:text-white relative ">
         {children}
         </main>
     </div>
